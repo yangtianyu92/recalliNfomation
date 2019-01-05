7cabeeea643014239a5e4bbe8335da51936a9927
888 <yangtianyu92@126.com>
2019-01-05 16:59:16 +0800

    hi

---
+++ change_tables.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/5 15:57
+# @Email    : yangtianyu92@126.com
+import pymysql.cursors
+from linkmysql import link_mysql_read, link_mysql_write
+# 链接数据库
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='root',
+                             db='test',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+# 增加字段语句
+sql = """
+alter table {} add (
+    `title` varchar(1200) DEFAULT NULL,
+    `product` varchar(300) DEFAULT NULL,
+    `brands` varchar(300) DEFAULT NULL,
+    `Manufacturer` varchar(300) DEFAULT NULL,
+    `Packageranddistributor` varchar(300) DEFAULT NULL,
+    `Numbers` int(15) DEFAULT NULL,
+    `html` MediumText DEFAULT NULL
+);
+"""
+
+sql_show_all = "show tables;"
+# 返回所有的数据库内的表名
+table_names = [name["Tables_in_test"] for name in link_mysql_read(sql_show_all)]
+
+for table_name in table_names:
+    link_mysql_write(sql.format(table_name))
+
+print("ok")
--- linkmysql.py
+++ linkmysql.py
@@ -12,22 +12,44 @@ connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
+
 # 读取数据库
 def link_mysql_read(sql):
     try:
         with connection.cursor() as cursor:
             cursor.execute(sql)
+            result = cursor.fetchall()
     finally:
         connection.close()
+    return result
+
 
 # 写入数据库或更改数据库
 def link_mysql_write(sql):
     try:
         with connection.cursor() as cursor:
             cursor.execute(sql)
+        result = cursor.fetchall()
         connection.commit()
     finally:
         connection.close()
+    return result
+
 
 if __name__ == '__main__':
-    
+    sql_show_all = "show tables;"
+    table_names = [name["Tables_in_test"] for name in link_mysql_read(sql_show_all)]
+    print(table_names[0])
+    sql_test = """
+    alter table agri_hotspot add (
+        `title` varchar(1200) DEFAULT NULL,
+        `product` varchar(300) DEFAULT NULL,
+        `brands` varchar(300) DEFAULT NULL,
+        `Manufacturer` varchar(300) DEFAULT NULL,
+        `Packageranddistributor` varchar(300) DEFAULT NULL,
+        `Numbers` int(15) DEFAULT NULL,
+        `html` MediumText DEFAULT NULL
+    );
+    """
+    link_mysql_write(sql_test)
+
