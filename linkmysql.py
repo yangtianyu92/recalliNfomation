632f21ab6073039074f314c86875e2c275e3935d
888 <yangtianyu92@126.com>
2019-01-05 17:21:29 +0800

    hi

--- linkmysql.py
+++ linkmysql.py
@@ -20,7 +20,7 @@ def link_mysql_read(sql):
             cursor.execute(sql)
             result = cursor.fetchall()
     finally:
-        connection.close()
+        print('ok')
     return result
 
 
@@ -29,19 +29,24 @@ def link_mysql_write(sql):
     try:
         with connection.cursor() as cursor:
             cursor.execute(sql)
-        result = cursor.fetchall()
         connection.commit()
+    except:
+        raise BufferError
     finally:
-        connection.close()
-    return result
+        print('ok')
 
 
 if __name__ == '__main__':
     sql_show_all = "show tables;"
     table_names = [name["Tables_in_test"] for name in link_mysql_read(sql_show_all)]
-    print(table_names[0])
-    sql_test = """
-    alter table agri_hotspot add (
+    print(table_names[1])
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='root',
+                                 db='test',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    sql_test = """alter table agri_policy add (
         `title` varchar(1200) DEFAULT NULL,
         `product` varchar(300) DEFAULT NULL,
         `brands` varchar(300) DEFAULT NULL,
@@ -49,7 +54,6 @@ if __name__ == '__main__':
         `Packageranddistributor` varchar(300) DEFAULT NULL,
         `Numbers` int(15) DEFAULT NULL,
         `html` MediumText DEFAULT NULL
-    );
-    """
+    );"""
     link_mysql_write(sql_test)
 
